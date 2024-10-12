36ace2abbb5a2d9ddd01c6ba5429fc601c6b4ad3
BlackCat <ido.ji.stats@gmail.com>
2024-10-13 00:20:08 +0900

    Update 1.Smoothing by B-spline with Second derivative penalty.R

--- 2.Construction of curves by distance/4.Smoothing curves by B-spline basis expansion/1.Smoothing by B-spline with Second derivative penalty.R
+++ 2.Construction of curves by distance/4.Smoothing curves by B-spline basis expansion/1.Smoothing by B-spline with Second derivative penalty.R
@@ -36,6 +36,20 @@ path_FC = path_FC %>% adjust_path
 path_export = "/Volumes/ADNI_SB_SSD_NTFS_4TB_Sandisk/Smoothing_FunImgARglobalCWSF_FC"
 path_export = path_export %>% adjust_path
 
+
+
+### 🟧 AAL3  ====================================================================================================================
+path_atlas = list.files(path_FC, "AAL3.rds", full.names = T)
+results = smoothing_multiple_ROIs(path_atlas, 
+                                  n_order = 4,
+                                  n_breaks = 300,
+                                  lambdas = exp(seq(-5, -4, 0.1)),  # 후보 smoothing 파라미터
+                                  path_export,
+                                  save_each_ROI = F,
+                                  overwrite = T)
+
+
+
 ### 🟧 Schaefer2018_1000Parcels_17Networks_order_FSLMNI152__resampled.nii.gz_ =====================================================================
 path_atlas = list.files(path_FC, "Schaefer2018_1000Parcels_17Networks_order_FSLMNI152__resampled.nii.gz.rds", full.names = T)
 results = smoothing_multiple_ROIs(path_atlas, 
@@ -46,14 +60,6 @@ results = smoothing_multiple_ROIs(path_atlas,
                                   save_each_ROI = T)
 
 
-### 🟧 AAL3  ====================================================================================================================
-path_atlas = list.files(path_FC, "AAL3.rds", full.names = T)
-results = smoothing_multiple_ROIs(path_atlas, 
-                                  n_order = 4,
-                                  n_breaks = 300,
-                                  lambdas = exp(seq(-5, -4, 0.1)),  # 후보 smoothing 파라미터
-                                  path_export,
-                                  save_each_ROI = F)
 
 
 
