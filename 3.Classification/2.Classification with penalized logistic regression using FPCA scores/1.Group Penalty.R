e75b554ea96c1980cd9205b50db033d3c96142dd
BlackCat <ido.ji.stats@gmail.com>
2024-11-04 22:01:16 +0900

    Update 1.Group Penalty.R

--- 3.Classification/2.Classification with penalized logistic regression using FPCA scores/1.Group Penalty.R
+++ 3.Classification/2.Classification with penalized logistic regression using FPCA scores/1.Group Penalty.R
@@ -1,58 +1,45 @@
 # 🟩 Load data =========================================================================================================
-path_data_list = "/Users/Ido/Library/CloudStorage/Dropbox/✴️DataAnalysis/FunCurv/3.Classification/1.FPCA"
-path_all_data_list = list.files(path_data_list, full.names = T)
-path_save = "/Volumes/ADNI_SB_SSD_NTFS_4TB_Sandisk/FunCurv/3.Classification/2.Group Penalty"
-
-
-
-
-
-
+path_data_list <- "E:/FunCurv/3.Classification/1.FPCA"
+path_all_data_list <- list.files(path_data_list, full.names = TRUE)
+path_save <- "E:/FunCurv/3.Classification/2.Classification with penalized logistic regression using FPCA scores/1.Group Penalty"
 
 # 🟨 Group Penalty =========================================================================================================
-alphas = seq(0.1, 1, by = 0.1)  # alpha: 0.1에서 1까지 0.1 간격
-lambdas = 10^seq(3, -3, length.out = 100)  # lambda: log scale에서 100개의 값
-penalties = c("grLasso", "grMCP", "grSCAD", "gel", "cMCP")
+alphas <- seq(0.1, 1, by = 0.1)  # alpha: 0.1에서 1까지 0.1 간격
+lambdas <- 10^seq(3, -3, length.out = 100)  # lambda: log scale에서 100개의 값
+penalties <- c("grLasso", "grMCP", "grSCAD", "gel", "cMCP")
+
+
 
 for(path_ith_data in path_all_data_list){
-  # path_ith_data = path_all_data_list[2]
-  ith_data = basename(path_ith_data)
-  path_save_new = file.path(path_save, ith_data)
-  dir.create(path_save_new, showWarnings = F)
-  exported_file = file.path(path_save_new, "classification results.rds")
-  if(file.exists(exported_file)){
-    next
-  }
+  # path_ith_data = path_all_data_list[19]
+  ith_data <- basename(path_ith_data)
+  path_save_new <- file.path(path_save, ith_data)
+  if (!dir.exists(path_save_new)) dir.create(path_save_new, showWarnings = FALSE)
   
-  # Load the data
-  path_ith_data_fpca = list.files(path_ith_data, full.names = T, pattern = "fpca_scores")
-  fpca_scores = readRDS(path_ith_data_fpca)
-  fpca_scores_train = fpca_scores$train
-  fpca_scores_test = fpca_scores$test
-  fpca_scores_rep = fpca_scores$rep
+  # Load the datax
+  path_ith_data_fpca <- list.files(path_ith_data, full.names = TRUE, pattern = "fpca_scores")
+  fpca_scores <- readRDS(path_ith_data_fpca)
+  fpca_scores_train <- fpca_scores$train
+  fpca_scores_test <- fpca_scores$test
+  fpca_scores_rep <- fpca_scores$rep
   
   # Model fitting
-  opt_models_list = lapply(penalties, function(penalty){
-    find_opt_model_by_cv_grpreg(fpca_scores_train,
-                                fpca_scores_test,
-                                fpca_scores_rep,
-                                alphas,
-                                lambdas,
-                                penalty = penalty)
+  for(penalty in penalties){
+    # penalty = penalties[1]
+    path_exported_file <- file.path(path_save_new, paste0(penalty, ".rds"))
+    if(file.exists(path_exported_file)){
+      next
+    }
     
-  }) %>% setNames(penalties)
-  
-  
-  # save the results
-  saveRDS(opt_models_list, exported_file)
-  
+    # Optimize model with cross-validation
+    opt_models <- find_opt_model_by_cv_grpreg(fpca_scores_train,
+                                              fpca_scores_test,
+                                              fpca_scores_rep,
+                                              alphas,
+                                              lambdas,
+                                              penalty = penalty)
+    # Save the results
+    saveRDS(opt_models, path_exported_file)
+  }
 }
-
-
-
-
-
-
-
-
 
